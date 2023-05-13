a6deb7c5dcd64186d475ef99b808fcc49d0469ce
Fernando Moreira <oakley-hbs@hotmail.com>
2023-05-13 17:28:36 -0300

    switch from leaflet to google maps for stability

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,86 +1,83 @@
-import { useState, useEffect, useRef } from 'react';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import React, { useState, useEffect, useRef } from 'react';
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import MapTheme from "./MapTheme.json";
 
-
-import CustomMarker from './components/Marker';
-
-// Data
-import markersData from "../media/markers_data.json";
-
+import StationMarker from './components/Marker';
 import StationDetails from "./components/Details/Details";
 import Localizator from "./components/Localizator";
 import Searchbar from "./components/SearchBar";
 
-export default function Map() {
-    // map variables
-    const [style] = useState("light_all");
-    const [zoom] = useState(15);
-    const [scale] = useState("@2x");
+import markersData from "../media/markers_data.json";
 
-    // hooks
-    const [showDetails, setShowDetail] = useState(false);
-    const [currentCordinates, setCurrentCordinates] = useState(null);
-    const mapRef = useRef(null);
+const mapOptions = {
+  styles: MapTheme,
+  disableDefaultUI: true,
+  fullscreenControl: false,
+  clickableIcons: false,
+  streetViewControl: false
+};
 
-    // events
-    const handleMarkerClick = (data) => {
-        console.log(`Marker clicked at ${data.name}`);
-        setShowDetail(true);
-    };
+function Map() {
+  const [showDetails, setShowDetail] = useState(false);
+  const [currentCordinates, setCurrentCordinates] = useState({});
+  const mapRef = useRef(null);
 
-    const handleLocalizeMe = () => {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition((position) => {
-                const { latitude, longitude } = position.coords;
-                setCurrentCordinates([latitude, longitude]);
-                updateMapView();
+  const GetCurrentCoords = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const _coords = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
 
-                console.log(currentCordinates);
-            }, () => {
-                console.log('Não foi possível obter a localização atual.');
-            });
-        }
+        setCurrentCordinates(_coords);
+      }, () => {
+        console.log('Não foi possível obter a localização atual.');
+      });
     }
+  }
 
-    const updateMapView = () => {
-        if (mapRef.current) {
-            mapRef.current.setView(currentCordinates, zoom, {
-                animate: mapRef.current
-            });
-        }
-    }
+  useEffect(() => {
+    GetCurrentCoords();
+  }, [])
 
-    useEffect(() => {
-        handleLocalizeMe();
-    }, []); 
 
-    return (
-        <>
-            <div className='overlay-container disable-pointer'>
-                <Searchbar />
-                <Localizator locateEvent={() => { handleLocalizeMe() }} />
-                <StationDetails
-                    show={showDetails}
-                    backEvent={() => { setShowDetail(false) }}
-                    locateEvent={() => { handleLocalizeMe() }}
-                />
-            </div>
+  const HandleMarkerClick = (data) => {
+    console.log(data.name);
+  }
 
-            {
-                currentCordinates && (
-                    <MapContainer ref={mapRef} center={currentCordinates} zoom={zoom} zoomControl={false} doubleClickZoom={false} >
-                        {/* <TileLayer url={`https://{s}.basemaps.cartocdn.com/${style}/{z}/{x}/{y}${scale}.png`} /> */}
-                        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-                        {/* <TileLayer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png" /> */}
-                        {/* <TileLayer url="https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}" /> */}
+  return (
+    <>
+      <div className='overlay-container disable-pointer'>
+        <Searchbar />
+        <Localizator locateEvent={() => { GetCurrentCoords() }} />
+        <StationDetails
+          show={showDetails}
+          backEvent={() => { setShowDetail(false) }}
+          locateEvent={() => {
+            mapRef.current.panTo(currentCordinates)
+          }}
+        />
+      </div>
 
-                        
-                        {markersData.markers.map((data) => (
-                            <CustomMarker key={data.id} markerData={data} eventHandlers={{ click: (e) => { handleMarkerClick(data) } }} />
-                        ))}
-                    </MapContainer>
-                )
-            }
-        </>
-    );
+      <LoadScript googleMapsApiKey={process.env.REACT_APP_MAPS_API_KEY}>
+        <GoogleMap
+          mapContainerStyle={{ width: '100vw', height: '100vh' }}
+          center={currentCordinates} zoom={15}
+          options={mapOptions}
+          onLoad={(map) => { mapRef.current = map; }}
+        >
+          {markersData.markers.map((m) => (
+            <StationMarker
+              key={m.id}
+              position={{ lat: m.location.lat, lng: m.location.lng }}
+              onMarkerClick={() => { HandleMarkerClick(m) }}
+            />
+          ))}
+        </GoogleMap>
+      </LoadScript>
+    </>
+  );
 }
+
+export default React.memo(Map);
--- src/pages/components/Marker.js
+++ src/pages/components/Marker.js
@@ -1,38 +1,20 @@
-import { useEffect, useRef, useState } from 'react';
-import { Marker } from 'react-leaflet';
-import L from "leaflet";
-import MarkerIcon from "../../media/map/map_marker.png";
+import React from 'react';
 
-export default function CustomMarker(props) {
-  const [markerIcon, setMarkerIcon] = useState(null);
-  const [markerData, setmarkerData] = useState(null);
+import { MarkerF } from '@react-google-maps/api';
 
-  useEffect(() => {
-    setmarkerData(props.markerData);
-
-    // Load custom marker icon
-    const customIcon = L.icon({
-      iconUrl: MarkerIcon,
-      iconSize: [40, 45],
-      iconAnchor: [40 / 2, 45],
-      className: 'map-marker-custom'
-    });
-
-    setMarkerIcon(customIcon);
-  }, []);
+export default function StationMarker(props) {
+  const markerOptions = {
+    icon: {
+      url: require("../../media/map/map_marker.png"),
+      scaledSize: new window.google.maps.Size(30, 40),
+    }
+  };
 
   return (
-    <>
-      { markerIcon &&
-        <Marker
-          position={[
-            markerData.location.lat,
-            markerData.location.lng
-          ]}
-          icon={markerIcon}
-          eventHandlers={props.eventHandlers}
-        />
-      }
-    </>
+    <MarkerF
+      position={props.position}
+      options={markerOptions}
+      onClick={props.onMarkerClick}
+    />
   );
 }
